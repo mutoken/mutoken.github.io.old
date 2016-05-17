5abcf8e3586ea80d879e3b66d74f62b5e3285983
Kentaro Muto <kentaro.muto@gmail.com>
2016-05-17 07:16:44 +0000

    create message model

---
+++ db/migrate/20160517063229_create_messages.rb
@@ -0,0 +1,10 @@
+class CreateMessages < ActiveRecord::Migration
+  def change
+    create_table :messages do |t|
+      t.string :name
+      t.string :body
+
+      t.timestamps null: false
+    end
+  end
+end
