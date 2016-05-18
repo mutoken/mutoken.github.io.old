7a8eefb18bc5f4ccf019e3cb65e0b57b58d00240
Kentaro Muto <kentaro.muto@gmail.com>
2016-05-18 14:29:56 +0000

    using Bootstrap

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,21 @@
 class MessagesController < ApplicationController
   def index
+    @message = Message.new
+    ##Messagesを全て取得する
+    @messages = Message.all
   end
+  
+  def create
+    @message = Message.new(message_params)
+    @message.save
+    redirect_to root_path , notice: 'メッセージを保存しました'
+  end
+  
+  private
+  def message_params
+    params.require(:message).permit(:name,:body)
+    ## ここまで
+  end
+  
+
 end
